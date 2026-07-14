306a1c5a7b1192e4e9e284c0a679b7fda2ce6a5f
miaoyin <wangyq0616@gmail.com>
2026-07-14 17:23:08 +0800

    feat:improve file tools

--- fileutil/encoding.go
+++ fileutil/encoding.go
@@ -2,11 +2,12 @@ package fileutil
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 	"path/filepath"
-    "strings"
+	"strings"
 
-    "github.com/pelletier/go-toml/v2"
+	"github.com/pelletier/go-toml/v2"
 	"gopkg.in/yaml.v3"
 )
 
@@ -14,45 +15,58 @@ import (
 // encoding: support map, list
 
 
+//Unmarshal 解析
+// ext后缀
+func Unmarshal(ext string, src []byte, v any) error {
+	ext = strings.TrimLeft(ext, ".")
+	switch ext {
+	case "yaml", "yml":
+		return yaml.Unmarshal(src, v)
+	case "json":
+		return json.Unmarshal(src, v)
+	case "toml":
+		return toml.Unmarshal(src, v)
+	default:
+		return fmt.Errorf("unknown extension: %s", ext)
+	}
+}
+
+func Marshal(ext string, v any) ([]byte, error) {
+	ext = strings.TrimLeft(ext, ".")
+	switch ext {
+	case "yaml", "yml":
+		return yaml.Marshal(v)
+	case "json":
+		return json.Marshal(v)
+	case "toml":
+		return toml.Marshal(v)
+	default:
+		return nil, fmt.Errorf("unknown extension: %s", ext)
+	}
+}
+
 // FileUnmarshal 读文件并解码
 func FileUnmarshal(path string, v any) error {
 	dataBytes, err := os.ReadFile(path)
 	if err != nil {
 		return err
 	}
-	ext := filepath.Ext(path)
-	switch ext {
-	case ".yaml", ".yml":
-		return yaml.Unmarshal(dataBytes, v)
-	case ".json":
-		return json.Unmarshal(dataBytes, v)
-	case ".toml":
-		return toml.Unmarshal(dataBytes, v)
-	default:
-		return json.Unmarshal(dataBytes, v)
-	}
+	return Unmarshal(filepath.Ext(path), dataBytes, v)
 }
 
-// MarshalByExt 编码
+// MarshalByPath 编码
 //    path 文件路径或者后缀
 //    v 数据对象
-func MarshalByExt(path string, v any) ([]byte, error) {
-    ext := strings.TrimLeft(filepath.Ext(path), ".")
-    switch ext {
-    case "yaml", "yml":
-        return yaml.Marshal(v)
-    case "json":
-        return json.Marshal(v)
-    case "toml":
-        return toml.Marshal(v)
-    default:
-        return json.Marshal(v)
-    }
+func MarshalByPath(path string, v any) ([]byte, error) {
+	return Marshal(filepath.Ext(path), v)
 }
 
 // FileMarshal 编码并保存文件
 func FileMarshal(path string, v any) (err error) {
-    dataBytes, err := MarshalByExt(path, v)
+    dataBytes, err := MarshalByPath(path, v)
+	if err != nil {
+		return err
+	}
 	return os.WriteFile(path, dataBytes, os.ModePerm)
 }
 
